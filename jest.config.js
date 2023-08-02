685e09d411c15d17fc8489d4aa9692d7ee7a6367
songforthemute <huni0423@naver.com>
2023-08-02 18:09:56 +0900

    [ADD] Lint Rule
    - Add Prettier & ESLint configure

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 /** @type {import('jest').Config} */
 const config = {
-    testEnvironment: "node", // 테스트 환경 지정
-    testMatch: ["**/tests/*.test.js"],
-    setupFiles: ["dotenv/config"],
+  testEnvironment: "node", // 테스트 환경 지정
+  testMatch: ["**/tests/*.test.js"],
+  setupFiles: ["dotenv/config"],
 };
 
 module.exports = config;
