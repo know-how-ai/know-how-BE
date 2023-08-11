13c2ef8ccda9ee7d3bd3d0f548d4e41ba4066418
errorcoder <kjw8971@naver.com>
2023-08-11 19:30:54 +0900

    [ADD] API 라우팅 구체화
    [ADD] 요청, 응답, 프롬프트 작성

--- routes/kogpt.js
+++ routes/kogpt.js
@@ -1,19 +1,60 @@
 const express = require("express");
 const router = express.Router();
 
-// GET /user/coverletter 라우터
-router.get("/coverletter", (req, res) => {
-  return res.send("hello, coverletter!");
+// user/coverletter 라우터
+router.post("/coverletter", (req, res) => {
+  const { coverletter, job } = req.body;
+  const { id } = req.session;
+
+  const prompt =
+    `${job}에 지원하고자 자기소개서를 다음과 같이 작성했습니다.` +
+    coverletter +
+    "이 자기소개서에서 잘한 점 3가지와 못한 점 3가지를 찾고, 총평을 작성해주세요.";
+
+  return res.status(200).json({
+    status: true,
+    data: {
+      good: ["1. ...", "2. ...", "3. ..."],
+      bad: ["1. ...", "2. ...", "3. ..."],
+      overall: "So, ...",
+    },
+  });
 });
 
-// GET /user/job 라우터
-router.get("/job", (req, res) => {
-  return res.send("hello, job!");
+// user/job 라우터
+router.post("/job", (req, res) => {
+  const { job, domain, project, description, skill, feature } = req.body;
+  const { id } = req.session;
+
+  const prompt =
+    `${domain} 업계의 ${job}으로 취업하려고 합니다.` +
+    `${description} 내용의 ${project}를` +
+    `${skill} 등을 이용하여 진행했으며, ${feature}를 이뤄냈습니다.` +
+    `면접 전형에서 받을 가능성이 높은 질문에 5개를 작성해주세요.`;
+
+  return res.status(200).json({
+    status: true,
+    data: {
+      questions: ["1. ...", "2. ...", "3. ...", "4. ...", "5. ..."],
+    },
+  });
 });
 
-// GET /user/interview 라우터
-router.get("/interview", (req, res) => {
-  return res.send("hello, interview!");
+// user/interview 라우터
+router.post("/interview", (req, res) => {
+  const { personalities } = req.body;
+  const { id } = req.session;
+
+  const prompt =
+    `${personalities}의 성향을 가진 사람에게` +
+    "어울리는 직업 5가지를 각각 해당 직업에서 중요한 성향과 함께 추천해주세요.";
+
+  return res.status(200).json({
+    status: true,
+    data: {
+      recommendations: ["1. ...", "2. ...", "3. ...", "4. ...", "5. ..."],
+    },
+  });
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,24 +1,94 @@
 const express = require("express");
 const router = express.Router();
 
-// GET /user/new 라우터
-router.get("/new", (req, res) => {
-  return res.send("hello, new!");
+// user/new 라우터
+router.post("/new", (req, res) => {
+  const { email, password, username, resetQuestion, resetAnswer } = req.body;
+
+  return res.status(200).json({
+    status: true,
+    data: {
+      id: 0,
+      username: "hi",
+      point: 5,
+    },
+  });
 });
 
-// GET /user/in 라우터
-router.get("/in", (req, res) => {
-  return res.send("hello, in!");
+// user/in 라우터
+router.post("/in", (req, res) => {
+  const { email, password } = req.body;
+
+  return res.status(200).json({
+    status: true,
+    data: {
+      id: 1,
+      username: "hi~",
+      point: 5,
+    },
+  });
 });
 
-// GET /user/out 라우터
-router.get("/out", (req, res) => {
-  return res.send("hello, out!");
+// user/out 라우터
+router.post("/out", (req, res) => {
+  const { id } = req.session;
+
+  return res.status(200).json({
+    status: true,
+  });
 });
 
-// GET /user/reset 라우터
-router.get("/reset", (req, res) => {
-  return res.send("hello, reset!");
+// user/reset 라우터
+router
+  .route("/reset")
+  // POST, 이메일 검증
+  .post((req, res) => {
+    const { email } = req.body;
+
+    res.status(200).json({
+      status: true,
+      data: {
+        email: "id@example.com",
+        resetQuestion: "Where are you from?",
+      },
+    });
+  })
+  // PUT, 답변 검증 & 패스워드 변경
+  .put((req, res) => {
+    const { email, newPassword, resetAnswer } = req.body;
+
+    return res.status(200).json({
+      status: true,
+    });
+  });
+
+// GET /user/log 라우터
+router.get("/log", (req, res) => {
+  const { id } = req.session;
+  const { skip } = req.query;
+
+  return res.status(200).json({
+    status: true,
+    data: {
+      logs: [
+        {
+          createdAt: Date.now() - 2000,
+          comment: "최초 로그인",
+          amount: 5,
+        },
+        {
+          createdAt: Date.now() - 20000,
+          comment: "서비스 이용",
+          amount: -1,
+        },
+        {
+          createdAt: Date.now() - 200000,
+          comment: "최초 로그인",
+          amount: 5,
+        },
+      ],
+    },
+  });
 });
 
 module.exports = router;
