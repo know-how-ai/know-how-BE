4af4732713caf083528ee3949937e7870ff510dd
songforthemute <huni0423@naver.com>
2023-08-19 20:42:43 +0900

    [CHORE] Refactoring User Router

--- routes/user.js
+++ routes/user.js
@@ -12,28 +12,31 @@ const {
 } = require("../controllers/point");
 const { getTodayDate } = require("../libs/date");
 const { hashValue, compareHashed } = require("../libs/hash");
-const { isNotPrivate, isPrivate } = require("../middlewares/private");
+const { isPrivate, isNotPrivate } = require("../middlewares/private");
 
 //  /user/new 라우터
 router.post("/new", isNotPrivate, async (req, res, next) => {
-  const { email, password, username, resetQuestion, resetAnswer } = req.body;
+  const {
+    body: { email, password, username, resetQuestion, resetAnswer },
+  } = req;
 
   passport.authenticate("local", async (isError, user, error) => {
     if (isError) {
       return next(error);
     }
 
-    // 요청 이메일로 이미 가입된 사용자 있는지 검증
+    // 해당 이메일로 가입한 사용자가 있는지 검증
     if (user) {
       const error = "이미 가입된 이메일입니다.";
-      // 409 : Conflict, 요청-서버의 상태 간 충돌
+      // 409: Conflict, 요청-서버의 상태 간 충돌
       return res.status(409).json({
         status: false,
         error,
       });
     }
 
-    // 신규 유저 생성
+    // 패스워드 해싱 && 솔팅 - bcrypt
+    // 데이터베이스에 새 row 생성 - Users.create
     const newUser = await createUser({
       email,
       password: await hashValue(password),
@@ -48,11 +51,11 @@ router.post("/new", isNotPrivate, async (req, res, next) => {
       point: newUser.dataValues.point,
     };
 
-    // 최초 로그인 처리
+    // 최초 로그인으로 인한 포인트 지급 로그 생성 - PointLogs.create
     const newUserPointLog = await createPointLogByFirstLogin(
       newUser.dataValues.id,
     );
-
+    // 최초 로그인으로 인한 포인트 지급
     await updateUserByFirstLogin(
       newUser.dataValues.id,
       newUser.dataValues.point,
@@ -61,13 +64,13 @@ router.post("/new", isNotPrivate, async (req, res, next) => {
 
     data.point += newUserPointLog.dataValues.amount;
 
-    // 검증 로직 완료, 로그인 로직 수행
+    // 검증 로직 완료, 로그인 로직 수행 - sessioning
     return req.login(newUser, (loginError) => {
       if (loginError) {
         return next(loginError);
       }
 
-      // 201: Created, 요청에 따른 새로운 자원 생성
+      // 201: Created, 요청에 따른 새로운 자원 생성됨.
       return res.status(201).json({
         status: true,
         data,
@@ -78,7 +81,9 @@ router.post("/new", isNotPrivate, async (req, res, next) => {
 
 //  /user/in 라우터
 router.post("/in", isNotPrivate, async (req, res, next) => {
-  const { password } = req.body;
+  const {
+    body: { password },
+  } = req;
 
   passport.authenticate("local", async (isError, user, error) => {
     if (isError) {
@@ -116,8 +121,9 @@ router.post("/in", isNotPrivate, async (req, res, next) => {
     const today = getTodayDate();
     const isFirstLoggedInToday = today !== user.last_logged_in;
 
-    // 오늘의 최초 로그인 검증
+    // 오늘의 최초 로그인 - 최초 로그인 포인트 지급
     if (isFirstLoggedInToday) {
+      // 트랜잭션으로 DB 작업 단위를 묶을 수 있지 않을까?
       const newLog = await createPointLogByFirstLogin(user.id);
       await updateUserByFirstLogin(
         user.id,
@@ -129,12 +135,13 @@ router.post("/in", isNotPrivate, async (req, res, next) => {
       data.point += newLog?.dataValues?.amount;
     }
 
-    // 검증 로직 완료, 로그인 로직 수행
+    // 검증 로직 완료, 로그인 로직 수행 - sessioning
     return req.login(user, (loginError) => {
       if (loginError) {
         return next(loginError);
       }
 
+      // 로그인 완료
       return res.status(200).json({
         status: true,
         data,
@@ -152,6 +159,7 @@ router.post("/out", isPrivate, (req, res, next) => {
 
     req.session.destroy();
 
+    // 로그아웃 완료
     return res.status(200).json({
       status: true,
     });
@@ -163,7 +171,9 @@ router
   .route("/reset")
   // POST: 이메일 검증
   .post(isNotPrivate, async (req, res, next) => {
-    const { email } = req.body;
+    const {
+      body: { email },
+    } = req;
 
     try {
       // 유저 검색
@@ -186,18 +196,21 @@ router
           resetQuestion: user.dataValues.reset_question,
         },
       });
-    } catch (error) {
-      next(error);
+    } catch (err) {
+      next(err);
     }
   })
   // PUT: 패스워드 초기화 답변 검증 && 패스워드 변경
   .put(isNotPrivate, async (req, res, next) => {
-    const { email, newPassword, resetAnswer } = req.body;
+    const {
+      body: { email, newPassword, resetAnswer },
+    } = req;
+
     try {
-      // 유저 조회 - 패스워드 찾기 질답 검증
+      // 유저 조회 - 패스워드 초기화 답 매칭 검증
       const user = await selectUserByEmail(email);
       const isCorrect = resetAnswer === user.dataValues.reset_answer;
-      
+
       if (!isCorrect) {
         const error = "패스워드 찾기 질문의 답이 틀렸습니다.";
         // 401: Unauthorized, 요청된 리소스에 대해 유효하지 않은 인증 상태
@@ -207,7 +220,7 @@ router
         });
       }
 
-      // 유저의 패스워드 업데이트 Users.update
+      // 유저의 패스워드 업데이트 - Users.update
       await updateUserById(
         user.dataValues.id,
         "password",
@@ -217,16 +230,18 @@ router
       return res.status(200).json({
         status: true,
       });
-    } catch (error) {
-      next(error);
+    } catch (err) {
+      next(err);
     }
   });
 
 //  /user/log 라우터
 router.get("/log", isPrivate, async (req, res, next) => {
-  const { user } = req;
-  const { skip } = req.query;
-  
+  const {
+    user,
+    query: { skip },
+  } = req;
+
   try {
     const logsIntstance = await selectPointLogsBySkip(
       user.id,
@@ -243,8 +258,8 @@ router.get("/log", isPrivate, async (req, res, next) => {
         logs,
       },
     });
-  } catch (error) {
-    next(error);
+  } catch (err) {
+    next(err);
   }
 });
 
