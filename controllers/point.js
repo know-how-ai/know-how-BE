cdc4d2d2133bec878b923e244c30b4ae7d13c177
songforthemute <huni0423@naver.com>
2023-08-21 01:58:37 +0900

    [ADD] user controller `deleteUserById`
    - Add `deleteUserById(id: number)`.
    - Defined type limitation in all controllers.

--- controllers/point.js
+++ controllers/point.js
@@ -2,12 +2,30 @@ const PointLogs = require("../models/pointLogs");
 
 // 최초 로그인으로 인한 포인트 지급 로그 생성 - PointLogs.create
 const createPointLog = async (user_id, amount, comment) => {
+  if (
+    typeof user_id !== "number" ||
+    typeof amount !== "number" ||
+    typeof comment !== "string"
+  ) {
+    console.error(
+      "This function is need to number and string type. Please check the arguments.",
+    );
+    return;
+  }
+
   const result = await PointLogs.create({ user_id, amount, comment });
 
   return result;
 };
 
 const createPointLogByFirstLogin = async (user_id) => {
+  if (typeof user_id !== "number") {
+    console.error(
+      "This function is need to a number type. Please check the arguments.",
+    );
+    return;
+  }
+
   const point = 5;
   const comment = "최초 로그인으로 인한 포인트 지급";
 
@@ -20,6 +38,17 @@ const selectPointLogsBySkip = async (
   orderStyle,
   skip,
 ) => {
+  if (
+    typeof user_id !== "number" ||
+    typeof orderColumn !== "string" ||
+    typeof orderStyle !== "string"
+  ) {
+    console.error(
+      "This function is need to number and string type. Please check the arguments.",
+    );
+    return;
+  }
+
   const unit = 5; // 로그 검색 단위
   const offset = parseInt(skip);
 
