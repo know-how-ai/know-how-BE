4b94fa774cf7ca802d9310a74ee2d602df409dd7
songforthemute <huni0423@naver.com>
2023-08-23 15:45:31 +0900

    [FIX] Github Actions's setup for test

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,6 +2,16 @@ const request = require("supertest");
 const app = require("../app.js");
 
 describe("견본 테스트", () => {
+  beforeAll(() => {
+    // connect to database && create tables with models
+    require("../models")
+      .sequelize.sync({ alter: true })
+      .then((fulfilled) => {
+        console.log("DB 연결 성공. ✅");
+      })
+      .catch((err) => console.error(err));
+  });
+
   test("1 is 1", () => {
     expect(1).toBe(1);
   });
