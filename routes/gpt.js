e936c161b5ed23192e7538098d1a05910e5129ca
songforthemute <huni0423@naver.com>
2023-08-24 19:45:18 +0900

    [FIX] Feature `/gpt/*` routes

--- routes/gpt.js
+++ routes/gpt.js
@@ -1,129 +1,114 @@
 const express = require("express");
 const { isPrivate } = require("../middlewares/private");
 const router = express.Router();
-const { updateUserById } = require("../controllers/user");
-const { createPointLog } = require("../controllers/point");
 const { getCompletion } = require("../libs/openai");
+const isAffordablePoint = require("../middlewares/isAffordablePoint");
+const afterUsingGPT = require("../middlewares/afterUsingGPT");
 
 //  /user/coverletter 라우터
-router.post("/coverletter", isPrivate, async (req, res, next) => {
-  const {
-    user: { id, point },
-    body: { coverletter, job },
-  } = req;
+router.post(
+  "/coverletter",
+  isPrivate,
+  isAffordablePoint,
+  async (req, res, next) => {
+    const {
+      body: { coverletter, job },
+    } = req;
 
-  const prompt =
-    `${job}에 지원하고자 자기소개서를 다음과 같이 작성했습니다. ` +
-    coverletter +
-    ` 이 자기소개서에서 잘한 점 3가지, 아쉬운 점 3가지를 작성하고, 전체적인 총평을` +
-    ` {"result": {"good": ["1. ...", "2. ...", "3. ..."], "bad": ["1. ...", "2. ...", "3. ..."], "overall": ["..."] }}` +
-    ` 같은 JSON 형태로 작성해주세요.`;
+    const prompt =
+      `${job}에 지원하고자 자기소개서를 다음과 같이 작성했습니다. ` +
+      coverletter +
+      ` 이 자기소개서에서 잘한 점 3가지, 아쉬운 점 3가지를 작성하고, 전체적인 총평을` +
+      ` {"result": {"good": ["1. ...", "2. ...", "3. ..."], "bad": ["1. ...", "2. ...", "3. ..."], "overall": ["..."] }}` +
+      ` 같은 JSON 형태로 작성해주세요.`;
 
-  try {
-    // GPT 호출
-    // JSON 변환
-    // const prompt = "대한민국이라는 국가에 대해 간략하게 설명해주세요.";
-    const result = await getCompletion(prompt);
-    const json = JSON.parse(result?.message?.content);
+    try {
+      const result = await getCompletion(prompt);
+      const json = await JSON.parse(result?.message.content);
 
-    /**
-     * {
-     *  index: number,
-     *  message: {
-     *    role: string,
-     *    content: any
-     *  },
-     *  finish_reason: string
-     * }
-     */
-
-    // 유저 포인트 삭감
-    await updateUserById(id, "point", point - 1);
-    // 포인트 로그 기록
-    await createPointLog(id, -1, "자소서 코칭 서비스 이용으로 인한 차감");
-
-    return res.status(200).json({
-      status: true,
-      data: {
+      req.data = {
         result: json?.result,
-      },
-    });
-  } catch (err) {
-    next(err);
-  }
-});
+      };
 
-//  /user/job 라우터
-router.post("/job", isPrivate, async (req, res, next) => {
-  const {
-    user: { id, point },
-    body: { job, domain, project, description, skill, feature },
-  } = req;
-
-  const prompt =
-    `${domain} 업계의 ${job}으로 취업하려고 합니다.` +
-    `${description} 내용의 ${project}를` +
-    `${skill} 등을 이용하여 진행했으며, ${feature}를 이뤄냈습니다.` +
-    `면접 전형에서 받을 가능성이 높은 질문 5개를 ` +
-    `{"result" : ["1. ...", "2. ...", "3. ...", "4. ...", "5. ..."]}과 같은 JSON 형식으로 작성해주세요.`;
-
-  try {
-    // GPT 호출
-    // JSON 변환
-    // const prompt = "대한민국이라는 국가에 대해 간략하게 설명해주세요.";
-    const result = await getCompletion(prompt);
-    const json = JSON.parse(result?.message?.content);
-
-    // 유저 포인트 삭감
-    await updateUserById(id, "point", point - 1);
-    // 포인트 로그 기록
-    await createPointLog(id, -1, "면접 코칭 서비스 이용으로 인한 차감");
-
-    return res.status(200).json({
-      status: true,
-      data: {
-        result: json?.result,
-      },
-    });
-  } catch (err) {
-    next(err);
-  }
-});
+      next();
+    } catch (err) {
+      next(err);
+    }
+  },
+  afterUsingGPT,
+);
 
 //  /user/interview 라우터
-router.post("/interview", isPrivate, async (req, res, next) => {
-  const {
-    user: { id, point },
-    body: { personalities },
-  } = req;
+router.post(
+  "/interview",
+  isPrivate,
+  isAffordablePoint,
+  async (req, res, next) => {
+    const {
+      body: { job, domain, project, description, skill },
+    } = req;
 
-  const prompt =
-    `"${personalities}"의 성향을 가진 사람에게 ` +
-    "어울리는 직업과 해당 직업에서 그 성향이 중요한 이유와 함께 총 5가지를 " +
-    `{"result" : [{ "job": "...", "description": "..." }, { "job": "...", "description": "..." }, { "job": "...", "description": "..." }, { "job": "...", "description": "..." }, { "job": "...", "description": "..." }] } ` +
-    `같은 JSON 형태로 추천해주세요.`;
+    const prompt =
+      `'${domain}' 업계의 '${job}'으로 취업하려고 합니다.` +
+      `'${description}'와 같은 내용의 '${project}'를` +
+      `'${skill}' 등을 이용하여 진행했습니다.` +
+      `면접 전형에서 받을 가능성이 높은 질문 5개를 ` +
+      `{"result" : ["1. ...", "2. ...", "3. ...", "4. ...", "5. ..."]}과 같은 JSON 형식으로 작성해주세요.`;
 
-  try {
-    // GPT 호출
-    // JSON 변환
-    // const prompt = "대한민국이라는 국가에 대해 간략하게 설명해주세요.";
-    const result = await getCompletion(prompt);
-    const json = JSON.parse(result?.message?.content);
+    try {
+      const result = await getCompletion(prompt);
+      const json = await JSON.parse(result?.message.content);
 
-    // 유저 포인트 삭감
-    await updateUserById(id, "point", point - 1);
-    // 포인트 로그 기록
-    await createPointLog(id, -1, "직업 추천 서비스 이용으로 인한 차감");
-
-    return res.status(200).json({
-      status: true,
-      data: {
+      req.data = {
         result: json?.result,
-      },
-    });
-  } catch (err) {
-    next(err);
-  }
-});
+      };
+
+      next();
+    } catch (err) {
+      next(err);
+    }
+  },
+  afterUsingGPT,
+);
+
+//  /user/job 라우터
+router.post(
+  "/job",
+  isPrivate,
+  isAffordablePoint,
+  async (req, res, next) => {
+    const {
+      body: { personalities },
+    } = req;
+
+    const str =
+      personalities.length === 1 ? personalities[0] : personalities.join(", ");
+
+    // const prompt =
+    //   `'${str}'의 성향을 가진 사람에게 ` +
+    //   "가장 추천할만한 직업과 사유 3가지를 " +
+    //   `{"result" : [{ "job": "...", "description": "..." }, ... ]} ` +
+    //   `와 같은 형태의 JSON 형식으로 작성해주세요.`;
+    const prompt =
+      `'${str}'의 성향을 가진 사람에게 ` +
+      "가장 추천할만한 직업과 적합한 근거를 덧붙인 답변 총 3가지를 " +
+      `{"result" : [{ "job": "...", "description": "..." }, ... ]} ` +
+      `와 같은 형태의 JSON 형식으로 작성해주세요.`;
+
+    try {
+      const result = await getCompletion(prompt);
+      const json = await JSON.parse(result?.message.content);
+
+      req.data = {
+        result: json?.result,
+      };
+
+      next();
+    } catch (err) {
+      return next(err);
+    }
+  },
+  afterUsingGPT,
+);
 
 module.exports = router;
