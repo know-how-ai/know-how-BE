7f7f1b73950bbe23bc5c66f9c0934d394e94be17
songforthemute <huni0423@naver.com>
2023-08-25 21:35:02 +0900

    [CHORE] Sample test is non-active

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,25 +2,12 @@ const request = require("supertest");
 const app = require("../app.js");
 
 describe("견본 테스트", () => {
-  beforeAll(() => {
-    // connect to database && create tables with models
-    require("../models")
-      .sequelize.sync({ alter: true })
-      .then(() => {
-        console.log("DB 연결 성공. ✅");
-      })
-      .catch((err) => console.error(err));
-  });
-
   test("1 is 1", () => {
     expect(1).toBe(1);
   });
 
-  test("App.js", () => {
-    return request(app)
-      .get("/")
-      .then((res) => {
-        expect(res.statusCode).toBe(200);
-      });
-  });
+  // test("App.js", async () => {
+  //   const res = await request(app).get("/");
+  //   expect(res.statusCode).toBe(200);
+  // });
 });
