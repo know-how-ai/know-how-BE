a5c4b6295d8d866c401b7729816be8ee987b977f
songforthemute <huni0423@naver.com>
2023-08-25 21:31:14 +0900

    [ADD] PreDeployment works
    - add redis(cloud).
    - add logging system w/ winston.
    - apply cross-env.
    - apply hpp, helmet for protection
    - apply pm2 for multi-processing

--- app.js
+++ app.js
@@ -1,26 +1,56 @@
 const express = require("express");
 const path = require("path");
-const dotenv = require("dotenv");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
-const cors = require("cors");
 const passport = require("passport");
 const passportConfig = require("./passport/index");
 const { errorHandler } = require("./middlewares/errorHandler");
+const helmet = require("helmet");
+const hpp = require("hpp");
+const redis = require("redis");
+const RedisStore = require("connect-redis").default;
 
 // for use env variables
-dotenv.config();
+require("dotenv").config();
 
+const redisClient = redis.createClient({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  password: process.env.REDIS_PASSWORD,
+});
+
+try {
+  redisClient.connect();
+} catch (err) {
+  console.warn(err);
+}
 const app = express();
 passportConfig(); // configure passport for auth
-const PORT = "port";
-const PORT_NUMBER = process.env.PORT_NUMBER || 3000;
 
-app.set(PORT, PORT_NUMBER);
+app.set("port", process.env.PORT || 8080);
+
+// connect to database && create tables with models
+require("./models")
+  .sequelize.sync({ force: false })
+  .then(() => {
+    console.log("DB 연결 성공. ✅");
+  })
+  .catch((err) => console.error(err));
 
 // for logging
-app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan("combined"));
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+      crossOriginEmbedderPolicy: false,
+      crossOriginResourcePolicy: false,
+    }),
+  );
+  app.use(hpp());
+} else {
+  app.use(morgan("dev"));
+}
 
 // for using static directory
 app.use("/", express.static(path.join(__dirname, "public")));
@@ -32,67 +62,53 @@ app.use(express.urlencoded({ extended: false }));
 // for parsing cookies
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-// for using session
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-      httpOnly: true,
-      secure: false,
-      signed: true,
-      maxAge: 24 * 60 * 60 * 1000, // a day
-    },
-    name: "express-session-cookie",
-  }),
-);
+const sessionOptions = {
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    signed: true,
+    maxAge: 24 * 60 * 60 * 1000, // a day
+  },
+  store: new RedisStore({ client: redisClient }), // set redis cache
+  name: "know-how",
+};
 
+if (process.env.NODE_ENV === "production") {
+  sessionOptions.proxy = true;
+  // sessionOptions.cookie.secure = true;
+}
+
+// for using session
+app.use(session(sessionOptions));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// for check current sessions
-// app.use((req, res, next) => {
-//   console.log("✅ cookies: ", req.cookies);
-//   console.log("✅ signedCookies: ", req.signedCookies);
-//   console.log("✅ session: ", req.session);
-//   console.log("✅ sessionID: ", req.sessionID);
-
-//   req.sessionStore.all((error, sessions) => {
-//     console.log("✅ sessionStore: ", sessions);
-//     next();
-//   });
-// });
-
 // for resolve 'cors' issue
-const corsMiddleware = cors({
-  origin: "http://localhost:3000",
-  optionsSuccessStatus: 200,
-});
+if (process.env.NODE_ENV !== "production") {
+  const cors = require("cors");
+  const corsMiddleware = cors({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  });
+
+  // GET / routing
+  app.get("/", corsMiddleware, (req, res) => {
+    return res.status(200).json({
+      message: "Hi!",
+      status: 200,
+    });
+  });
+}
 
 const { gptRouter, userRouter } = require("./routes");
 
-app.use(corsMiddleware);
+// app.use(corsMiddleware);
 app.use("/user", userRouter);
 app.use("/gpt", gptRouter);
 
-// GET / routing
-app.get("/", corsMiddleware, (req, res) => {
-  return res.status(200).json({
-    message: "Hi!",
-    status: 200,
-  });
-});
-
 // Error handling
 app.use(errorHandler);
-
-// connect to database && create tables with models
-require("./models")
-  .sequelize.sync({ alter: true })
-  .then((fulfilled) => {
-    console.log("DB 연결 성공. ✅");
-  })
-  .catch((err) => console.error(err));
 
 module.exports = app;
