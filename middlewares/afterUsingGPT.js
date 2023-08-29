38d7c99851ad1cf0a54b1a05dc77f06108297d89
songforthemute <huni0423@naver.com>
2023-08-29 17:39:17 +0900

    [FIX] Logging middleware
    - fixed used service's type

--- middlewares/afterUsingGPT.js
+++ middlewares/afterUsingGPT.js
@@ -5,13 +5,14 @@ const afterUsingGPT = async (req, res, next) => {
   const {
     user: { id, point },
     data,
+    gptType,
   } = req;
 
   try {
     // 유저 포인트 삭감
     await updateUserById(id, "point", point - 1);
     // 포인트 로그 기록
-    await createPointLog(id, -1, "직업 추천 서비스 이용으로 인한 차감");
+    await createPointLog(id, -1, gptType + " 서비스 이용으로 인한 차감");
 
     return res.status(200).json({
       status: true,
